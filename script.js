2e85ed7f481118705bf6e37014952548be3476ce
name <kia.tmg1@gmail.com>
2025-06-01 20:12:31 +0330

    RPS done

--- script.js
+++ script.js
@@ -1,71 +1,63 @@
 let ComputerChoices = ["Rock", "Paper", "Scissor"];
 let UserChoices = ["Rock", "Paper", "Scissor"];
 
-function getComputerChoice() {
-  return Math.floor(Math.random() * 3);
-}
-
 const pResult = document.querySelector(".pResult");
-pResult.textContent = getComputerChoice();
+const hResult = document.querySelector(".hResult");
+const hscoreresult = document.querySelector(".hscoreresult");
+const pscoreresult = document.querySelector(".pscoreresult");
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+const winner = document.querySelector(".winner");
 
-function getHumanChoice() {
-  const input = document.querySelector("input");
-  const button = document.querySelector("button");
-  const hResult = document.querySelector(".hResult");
-  button.addEventListener("click", () => {
-    const myItem = input.value;
-    input.value = "";
-    let choice = parseInt(myItem);
-    if (choice >= 0 && choice <= 2) {
-      console.log(`You chose: ${UserChoices[choice]}`);
-      const hResult = document.querySelector(".hResult");
-      hResult.textContent = choice;
-    } else {
-      console.log("wrong input");
-    }
-  });
-}
-
-function playgame() {
-  let ComputerScore = 0;
-  let UserScore = 0;
-
-  function playround(human, computer) {
-    if (UserChoices[human] === ComputerChoices[computer]) {
-      console.log("Tie,Same picks,Try Again!");
-    } else if (
-      (UserChoices[human] === "Rock" &&
-        ComputerChoices[computer] === "Scissor") ||
-      (UserChoices[human] === "Paper" &&
-        ComputerChoices[computer] === "Rock") ||
-      (UserChoices[human] === "Scissor" &&
-        ComputerChoices[computer] === "Paper")
-    ) {
-      console.log("User Won!");
-      UserScore++;
-      console.log(`UserScore: ${UserScore},ComputerScore:${ComputerScore}`);
-    } else if (
-      (UserChoices[human] === "Scissor" &&
-        ComputerChoices[computer] === "Rock") ||
-      (UserChoices[human] === "Rock" &&
-        ComputerChoices[computer] === "Paper") ||
-      (UserChoices[human] === "Paper" &&
-        ComputerChoices[computer] === "Scissor")
-    ) {
-      console.log("Computer Won!");
-      ComputerScore++;
-      console.log(`UserScore: ${UserScore},ComputerScore:${ComputerScore}`);
-    } else {
-      console.log("WTF just happend!");
-    }
+button.addEventListener("click", () => {
+  const myItem = input.value;
+  input.value = "";
+  let choice = parseInt(myItem);
+  if (choice < 0 || choice > 2) {
+    hResult.textContent = "wrong input";
+    return;
   }
-  for (let i = 0; i < 5; i++) {
-    console.log(`--- ROUND ${i + 1} ---`);
-    let user = getHumanChoice();
-    console.log(`User: ${user}`);
-    let computer = getComputerChoice();
-    console.log(`Pc: ${computer}`);
-    playround(user, computer);
+  let cpuChoice = Math.floor(Math.random() * 3);
+  hResult.textContent = choice;
+  pResult.textContent = cpuChoice;
+  playround(choice, cpuChoice);
+});
+
+let ComputerScore = 0;
+let UserScore = 0;
+
+function playround(human, computer) {
+  if (UserChoices[human] === ComputerChoices[computer]) {
+    winner.textContent = "Tie";
+  } else if (
+    (UserChoices[human] === "Rock" &&
+      ComputerChoices[computer] === "Scissor") ||
+    (UserChoices[human] === "Paper" && ComputerChoices[computer] === "Rock") ||
+    (UserChoices[human] === "Scissor" && ComputerChoices[computer] === "Paper")
+  ) {
+    console.log("User Won!");
+    UserScore++;
+    if (UserScore === 3) {
+      winner.textContent = "Human Won!";
+      button.disabled = true;
+    }
+    hscoreresult.textContent = UserScore;
+    console.log(`UserScore: ${UserScore},ComputerScore:${ComputerScore}`);
+  } else if (
+    (UserChoices[human] === "Scissor" &&
+      ComputerChoices[computer] === "Rock") ||
+    (UserChoices[human] === "Rock" && ComputerChoices[computer] === "Paper") ||
+    (UserChoices[human] === "Paper" && ComputerChoices[computer] === "Scissor")
+  ) {
+    console.log("Computer Won!");
+    ComputerScore++;
+    if (ComputerScore === 3) {
+      winner.textContent = "CPU Won!";
+      button.disabled = true;
+    }
+    pscoreresult.textContent = ComputerScore;
+    console.log(`UserScore: ${UserScore},ComputerScore:${ComputerScore}`);
+  } else {
+    winner.textContent = "wtf";
   }
 }
-playgame();
